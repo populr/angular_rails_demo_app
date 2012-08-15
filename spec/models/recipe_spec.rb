a8f11a2034cba2ce34844783cc59ffdd9ec4903c
Cade Truitt <ctruitt@centresource.com>
2012-08-14 20:44:23 -0500

    compact respond_to specs

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,58 +1,15 @@
 require 'spec_helper'
 
 describe Recipe do
-  let(:recipe) { Recipe.new }
   
-  it "should respond to name" do
-    recipe.should respond_to(:name)
-  end
+  describe "attributes" do
+    let(:recipe) { Recipe.new }
 
-  it "should respond to name=" do
-    recipe.should respond_to(:name=)
-  end
-
-  it "should respond to cook_time" do
-    recipe.should respond_to(:cook_time)
-  end
-
-  it "should respond to cook_time=" do
-    recipe.should respond_to(:cook_time=)
-  end
-
-  it "should respond to servings" do
-    recipe.should respond_to(:servings)
-  end
-
-  it "should respond to servings=" do
-    recipe.should respond_to(:servings=)
-  end
-
-  it "should respond to instructions" do
-    recipe.should respond_to(:instructions)
-  end
-
-  it "should respond to instructions=" do
-    recipe.should respond_to(:instructions=)
-  end
-
-  it "should respond to photo" do
-    recipe.should respond_to(:photo)
-  end
-
-  it "should respond to photo=" do
-    recipe.should respond_to(:photo=)
-  end
-
-  it "should respond to recipe_ingredients" do
-    recipe.should respond_to(:recipe_ingredients)
-  end
-
-  it "should respond to ingredients" do
-    recipe.should respond_to(:ingredients)
-  end
-  
-  it "should respond to owner" do
-    recipe.should respond_to(:owner)
+    %w(name name= cook_time cook_time= servings servings= instructions instructions= photo photo= recipe_ingredients ingredients owner).each do |meth|
+      it "should respond to #{meth}" do
+        recipe.should respond_to(meth.to_sym)
+      end
+    end
   end
   
   describe "validations" do
