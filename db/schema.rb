7d381f7cdc84e318697e33b3e7aff5f1e6bb5f7f
Cade Truitt <ctruitt@centresource.com>
2012-08-14 19:26:15 -0500

    round out ingredient attributes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120815001625) do
+ActiveRecord::Schema.define(:version => 20120815002307) do
+
+  create_table "ingredients", :force => true do |t|
+    t.string   "name",       :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "recipes", :force => true do |t|
     t.integer  "owner_id",           :null => false
--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -1,13 +1,22 @@
 require 'spec_helper'
 
-describe Ingredient, focus: true do
-
+describe Ingredient do
+  let(:ingredient) { Ingredient.new }
+  
   it "should respond to name" do
-    RecipeIngredient.new.should respond_to(:name)
+    ingredient.should respond_to(:name)
   end
 
   it "should respond to name=" do
-    RecipeIngredient.new.should respond_to(:name=)
+    ingredient.should respond_to(:name=)
+  end
+
+  it "should respond to recipe_ingredients" do
+    ingredient.should respond_to(:recipe_ingredients)
+  end
+
+  it "should respond to recipes" do
+    ingredient.should respond_to(:recipes)
   end
 
 end
